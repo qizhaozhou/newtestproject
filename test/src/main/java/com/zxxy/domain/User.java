dcb9fdb4e9d3b5f0ae946f989f50ebb2cd0c9e01
zhouqizhao <qizhaoa@163.com>
2020-09-30 14:19:38 +0800

    zhouqizhao

---
+++ test/src/main/java/com/zxxy/domain/User.java
@@ -0,0 +1,18 @@
+package com.zxxy.domain;
+
+/**
+ * @author: zhouqizhao
+ * @date: 2020/9/30 - 14:01
+ */
+public class User {
+
+    private String name="zhou";
+
+    private Integer age;
+
+    private String sex;
+
+
+
+
+}
